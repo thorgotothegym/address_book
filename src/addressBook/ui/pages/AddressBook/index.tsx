77660d4ce0d2ea9e42abf11be012ecbd4bd91dc1
Aleix Vidal <al3ix.vidal@gmail.com>
2022-07-21 21:36:47 +0200

    feat: implement Alert to display isFetching, error and display suggestions

--- src/addressBook/ui/pages/AddressBook/index.tsx
+++ src/addressBook/ui/pages/AddressBook/index.tsx
@@ -8,28 +8,72 @@ import {
   Radio,
   RadioGroup,
   Typography,
+  Input,
+  ListItemButton,
+  ListItemText,
 } from "@mui/material";
 import useFindCountries from "../../../infraestruture/hooks/queries/useFindCountries";
+import useFindSuggestions from "../../../infraestruture/hooks/queries/useFindSuggestions";
 import Country from "../../../domain/entities/Country";
 import { styles } from "./styles";
+import { PostCode } from "../../components/PostCode";
+import { Alert } from "../../components/Alert";
+import Suggestions from "../../../domain/entities/Suggestions";
 
 export enum SearchType {
   MANUAL = "manual",
   POSTALCODE = "postalCode",
 }
 
-export const AddressBook = (): JSX.Element => {
-  const { data } = useFindCountries();
+export type Address = {
+  line1: string;
+  line2: string;
+  line3: string;
+  town: string;
+  postcode: string;
+  country: string;
+};
 
+export const AddressBook = (): JSX.Element => {
   const [value, setValue] = useState<string>("postCode");
+  const [term, setTerm] = useState<string>("");
+
+  const [selectedAddress, setSelectedAddress] = useState<Address>({
+    country: "",
+    line1: "",
+    line2: "",
+    line3: "",
+    postcode: "",
+    town: "",
+  });
+
+  const [values, setValues] = useState<any>();
+
+  const { data } = useFindCountries();
+  const {
+    data: suggestion,
+    status,
+    isFetching,
+    error,
+  } = useFindSuggestions(term);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
     setValue((event.target as HTMLInputElement).value);
   };
 
+  const handleSelection = (address: string) => {
+    var match = address.split(", ");
+    let obj: any = selectedAddress;
+
+    Object.keys(obj).map((value, key) => (obj[value] = match[key]));
+
+    setSelectedAddress({ ...selectedAddress, ...obj });
+  };
+
   return (
     <Box sx={{ backgroundColor: "#f6f5ea" }}>
+      {JSON.stringify(selectedAddress)}
       <Grid container spacing={2} p={2}>
         <Grid item md={6}>
           <Box sx={styles.box}>
@@ -65,8 +109,69 @@ export const AddressBook = (): JSX.Element => {
                 />
               </RadioGroup>
             </FormControl>
-            {value === "manual" ? <>manual</> : null}
-            {value === "postCode" ? <>postCode</> : null}
+            <Box>
+              <Grid container>
+                <Grid xl={12}>
+                  {value === "manual" ? <>manual</> : null}
+                  {value === "postCode" ? (
+                    <>
+                      <Input
+                        sx={{ width: "100%" }}
+                        onChange={(event) => {
+                          setTerm(event.target.value);
+                        }}
+                        value={values}
+                      />
+                      <Box sx={{ width: "100%", paddingTop: "50px" }}>
+                        {status === "loading" ? (
+                          <Alert severity="info" message="Loading" />
+                        ) : status === "error" ? (
+                          <Alert severity="error" message={error.message} />
+                        ) : (
+                          <>
+                            {isFetching ? (
+                              <>
+                                <Alert severity="info" message="Fetching..." />
+                              </>
+                            ) : (
+                              <>
+                                {suggestion?.map(
+                                  ({ address }: Suggestions, key) => {
+                                    return (
+                                      <Box key={key}>
+                                        <ListItemButton component="a">
+                                          <ListItemText
+                                            onClick={() => {
+                                              handleSelection(address);
+                                            }}
+                                            secondary={
+                                              <React.Fragment>
+                                                <Typography
+                                                  sx={{ display: "inline" }}
+                                                  component="span"
+                                                  variant="body2"
+                                                  color="text.primary"
+                                                >
+                                                  {address}
+                                                </Typography>
+                                              </React.Fragment>
+                                            }
+                                          />
+                                        </ListItemButton>
+                                      </Box>
+                                    );
+                                  }
+                                )}
+                              </>
+                            )}
+                          </>
+                        )}
+                      </Box>
+                    </>
+                  ) : null}
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
         </Grid>
       </Grid>
