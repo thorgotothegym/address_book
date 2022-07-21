9dab9171811bfcbf220f7794f4efced0028c1c37
Aleix Vidal <al3ix.vidal@gmail.com>
2022-07-21 16:36:20 +0200

    feat: add CustomHeader to Layout

--- src/addressBook/ui/pages/Layout/index.tsx
+++ src/addressBook/ui/pages/Layout/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { CustomHeader } from "../../components/CustomHeader";
 
 import { AddressBook } from "../AddressBook";
 
 export const Layout = (): JSX.Element => {
   return (
     <>
+      <CustomHeader />
       <AddressBook />
     </>
   );
